8935bfa3f621d20bc18166566da3af08e0892387
rakeshdr543 <rakeshdr543@gmail.com>
2020-09-24 21:09:28 +0530

    Updates

---
+++ precache-manifest.3db4433e9fb02088616434e4956ded7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608681a16badb1665d0c5e1642afd3a9",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "7e83f4bb2e8908f87e18",
+    "url": "/movieapp/static/css/main.7d364d37.chunk.css"
+  },
+  {
+    "revision": "984c59f92f37f6bc90a1",
+    "url": "/movieapp/static/js/2.bf35b88c.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/movieapp/static/js/2.bf35b88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e83f4bb2e8908f87e18",
+    "url": "/movieapp/static/js/main.4136058e.chunk.js"
+  },
+  {
+    "revision": "0d516f28c241e3c70098",
+    "url": "/movieapp/static/js/runtime-main.04b21085.js"
+  }
+]);
